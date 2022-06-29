e8dfbb02a59675559228669a3ff7c98821c59347
Christoph Häni <christoph.haeni@bfh.ch>
2022-06-29 15:46:16 +0200

    Move to onLoad

--- R/setup.r
+++ R/setup.r
@@ -1,7 +1,15 @@
 
-# set base url
-baseurl <- "https://model.agrammon.ch/singleRest/api/v1"
-Sys.setenv(agrammon_rest_url = baseurl)
-# Sys.getenv('agrammon_rest_url')
+# loading namespace agrammon
+.onLoad <- function(libname, pkgname) {
+    # set agrammon rest api url
+    Sys.setenv(agrammon_rest_url =  "https://model.agrammon.ch/singleRest/api/v1")
+    invisible()
+}
 
+# unloading namespace agrammon
+.onUnload <- function(libpath) {
+    # unset agrammon rest api url
+    Sys.unsetenv('agrammon_rest_url')
+    invisible()
+}
 
